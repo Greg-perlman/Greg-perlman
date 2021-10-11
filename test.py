260c445ee8dba99e0197322d5a98a1dc6869cfc8
Anis <greg@gmail.com>
2021-10-11 15:42:55 +0200

    'first'

---
+++ test.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self, name, major, gpa):
+        self.name = name
+        self.major = major
+        self.gpa = gpa
+       
+
+
+student1 = Student('anis','math',3.0)
+
+print(student1.name)
